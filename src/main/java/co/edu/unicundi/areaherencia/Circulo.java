43fbddbdf8dc19551b97b4ce37a8f4f0109b2ecf
1Alejanra3 <palejandraguzman@ucundinamarca.edu.co>
2021-03-18 12:50:55 -0500

    calculo de circulo y variables de area

--- src/main/java/co/edu/unicundi/areaherencia/Circulo.java
+++ src/main/java/co/edu/unicundi/areaherencia/Circulo.java
@@ -1,14 +1,44 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package co.edu.unicundi.areaherencia;
+
+import static java.lang.Math.pow;
+import java.util.Scanner;
 
 /**
  *
  * @author User
  */
-public class Circulo {
+public class Circulo extends Area{
     
+    double radio;
+    static final double pi=3.1416;
+    
+    public void  cir(){
+        Scanner  entrada=new Scanner(System.in);
+        
+        System.out.println("Ingrese el radio del circulo");
+        radio=entrada.nextDouble();
+
+        area(radio,pi);
+        perimetro(radio,pi);
+        resultados();
+    }
+    
+    public double area(double radio,double pi)
+    {
+        super.area=pi*pow(radio,2);
+        return area;
+    }
+    
+    public double perimetro(double radio,double pi){
+        super.perimetro=2*pi*radio;
+        return perimetro;
+    }
+    
+    public void resultados(){
+        System.out.println("El area del ciruclo es: " + area(radio,pi));
+        System.out.println("El perimetro del circulo es: " + perimetro(radio,pi));
+    }
+    
+   
 }
--- src/main/java/co/edu/unicundi/areaherencia/Principal.java
+++ src/main/java/co/edu/unicundi/areaherencia/Principal.java
@@ -1,5 +1,7 @@
 package co.edu.unicundi.areaherencia;
 
+import java.util.Scanner;
+
 /**
  *
  * @author acer
@@ -7,5 +9,7 @@ package co.edu.unicundi.areaherencia;
 public class Principal {
     public static void main(String[] args) {
         
+        Circulo cir = new Circulo();
+        cir.cir();
     }
 }
